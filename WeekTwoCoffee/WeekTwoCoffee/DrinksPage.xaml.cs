eac47678eaab755f97315ccad05e9d57d659ec4e
DominicB-yte <53456554+DominicB-yte@users.noreply.github.com>
2021-04-25 16:56:06 +1200

    Initial commit

---
+++ WeekTwoCoffee/WeekTwoCoffee/DrinksPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeekTwoCoffee
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DrinksPage : ContentPage
+    {
+        public DrinksPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WeekTwoCoffee/WeekTwoCoffee/MainTabbedPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeekTwoCoffee
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTabbedPage : TabbedPage
+    {
+        public MainTabbedPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void drinksBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DrinksPage());
+        }
+
+        private async void breakfastBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BreakfastPage());
+        }
+
+        private async void brunchBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BrunchPage());
+        }
+
+        private async void lunchBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LunchPage());
+        }
+    }
+}
